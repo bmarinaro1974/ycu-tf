3c623d5883735fc4682ac01317ba4f1f1d695f78
Ekaterina Kuznetsova <github@technekate.com>
2016-04-01 15:51:13 -0700

    two more

---
+++ modules/ycu/notification.tf
@@ -0,0 +1,83 @@
+variable "Notification_ami_ids" {
+  description = ""
+  type        = "map"
+  default     = {
+    dev1=""
+    dev2=""
+    dev3=""
+    dev4=""
+    qa1=""
+    qa2=""
+    qa3=""
+    qa4=""
+    stage=""
+    prod=""
+  }
+}
+
+# Defines instance type for the hl7 autoscaling group.
+### NOTE: CHANGE IN environment-variables.tf AS AMIS ARE DEFINED ###
+
+variable "Notification_instance_types" {
+  description = ""
+  type        = "map"
+  default     = {
+    dev1=""
+    dev2=""
+    dev3=""
+    dev4=""
+    qa1=""
+    qa2=""
+    qa3=""
+    qa4=""
+    stage=""
+    prod=""
+  }
+}
+
+resource "template_file" "Notification_user_data" {
+  template = "${file("${path.module}/user-data.sh")}"
+
+  vars {
+    chef_role = "microservice-notification"
+    domain = "${var.domain}"
+    environment = "${var.environment}"
+    chef_boot_bucket = "${var.chef_boot_bucket}"
+    chef_config_bucket = "${var.chef_config_bucket}"
+    consul_bucket = "${var.consul_bucket}"
+    elasticsearch_boot_bucket = "${var.elasticsearch_boot_bucket}"
+  }
+}
+
+
+resource "aws_launch_configuration" "Notification_configuration" {
+  name                  = "${var.environment}_Notification"
+  image_id              = "${coalesce(lookup(var.Notification_ami_ids, var.environment), lookup(var.default_ami_ids, var.environment))}"
+  instance_type         = "${coalesce(lookup(var.Notification_instance_types, var.environment), lookup(var.default_instance_types, var.environment))}"
+  key_name              = "${var.instance_key_name}"
+  security_groups       = ["${aws_security_group.microservices.id}"]
+  iam_instance_profile  = "${aws_iam_instance_profile.microservices_profile.name}"
+  user_data             = "${template_file.Notification_user_data.rendered}"
+}
+
+
+resource "aws_autoscaling_group" "Notification_group" {
+  depends_on = ["aws_internet_gateway.ycu"]
+  depends_on = ["aws_autoscaling_group.Consul_group"]
+  #depends_on = ["aws_route.services_admin"]
+  vpc_zone_identifier = ["${aws_subnet.services.*.id}"]
+  name = "${var.environment}_Notification"
+  max_size = "${lookup(var.default_asg_max, var.environment)}"
+  min_size = "${lookup(var.default_asg_min, var.environment)}"
+  health_check_grace_period = "${var.default_asg_health_check_period}"
+  health_check_type = "${var.default_asg_health_check_type}"
+  desired_capacity = "${lookup(var.default_asg_desired, var.environment)}"
+  force_delete = true
+  launch_configuration = "${aws_launch_configuration.Notification_configuration.id}"
+
+  tag {
+    key = "Name"
+    value = "${var.environment}_Notification"
+    propagate_at_launch = true
+  }
+}
---
+++ modules/ycu/yci-50.tf
@@ -0,0 +1,82 @@
+variable "YCI-50_ami_ids" {
+  description = ""
+  type        = "map"
+  default     = {
+    dev1=""
+    dev2=""
+    dev3=""
+    dev4=""
+    qa1=""
+    qa2=""
+    qa3=""
+    qa4=""
+    stage=""
+    prod=""
+  }
+}
+
+# Defines instance type for the hl7 autoscaling group.
+### NOTE: CHANGE IN environment-variables.tf AS AMIS ARE DEFINED ###
+
+variable "YCI-50_instance_types" {
+  description = ""
+  type        = "map"
+  default     = {
+    dev1=""
+    dev2=""
+    dev3=""
+    dev4=""
+    qa1=""
+    qa2=""
+    qa3=""
+    qa4=""
+    stage=""
+    prod=""
+  }
+}
+
+resource "template_file" "YCI-50_user_data" {
+  template = "${file("${path.module}/user-data.sh")}"
+
+  vars {
+    chef_role = "microservice-yci-50"
+    domain = "${var.domain}"
+    environment = "${var.environment}"
+    chef_boot_bucket = "${var.chef_boot_bucket}"
+    chef_config_bucket = "${var.chef_config_bucket}"
+    consul_bucket = "${var.consul_bucket}"
+    elasticsearch_boot_bucket = "${var.elasticsearch_boot_bucket}"
+  }
+}
+
+resource "aws_launch_configuration" "YCI-50_configuration" {
+  name                  = "${var.environment}_YCI-50"
+  image_id              = "${coalesce(lookup(var.YCI-50_ami_ids, var.environment), lookup(var.default_ami_ids, var.environment))}"
+  instance_type         = "${coalesce(lookup(var.YCI-50_instance_types, var.environment), lookup(var.default_instance_types, var.environment))}"
+  key_name              = "${var.instance_key_name}"
+  security_groups       = ["${aws_security_group.microservices.id}"]
+  iam_instance_profile  = "${aws_iam_instance_profile.microservices_profile.name}"
+  user_data             = "${template_file.YCI-50_user_data.rendered}"
+}
+
+
+resource "aws_autoscaling_group" "YCI-50_group" {
+  depends_on = ["aws_internet_gateway.ycu"]
+  depends_on = ["aws_autoscaling_group.Consul_group"]
+  #depends_on = ["aws_route.services_admin"]
+  vpc_zone_identifier = ["${aws_subnet.services.*.id}"]
+  name = "${var.environment}_YCI-50"
+  max_size = "${lookup(var.default_asg_max, var.environment)}"
+  min_size = "${lookup(var.default_asg_min, var.environment)}"
+  health_check_grace_period = "${var.default_asg_health_check_period}"
+  health_check_type = "${var.default_asg_health_check_type}"
+  desired_capacity = "${lookup(var.default_asg_desired, var.environment)}"
+  force_delete = true
+  launch_configuration = "${aws_launch_configuration.YCI-50_configuration.id}"
+
+  tag {
+    key = "Name"
+    value = "${var.environment}_YCI-50"
+    propagate_at_launch = true
+  }
+}
