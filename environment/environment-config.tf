66ac3ed8ee0153d64dad0f87e6a386625064e705
joe.bolin@yourcareuniverse.com <joe.bolin@yourcareuniverse.com>
2016-04-04 06:46:32 -0400

    Added region and primary account for aws usage

--- environment/environment-config.tf
+++ environment/environment-config.tf
@@ -36,12 +36,12 @@ variable "workspace" {
   type = "string"
 }
 
+variable "primary_account" {
+  type = "string"
+}
 
-/*
 provider "aws" {
-  allowed_account_ids = ["${var.provider_primary.account}"]
-  access_key = "${var.provider_primary.access_key}"
-  secret_key = "${var.provider_primary.secret_key}"
+  allowed_account_ids = ["${var.primary_account}"]
   region     = "${var.region}"
 }
 */
@@ -52,7 +52,7 @@ module "application" {
   source = "../modules/ycu"
   environment = "${var.environment}"
   region = "${var.region}"
-/*  provider_primary_account = "${var.provider_primary.account}" */
+  provider_primary_account = "${var.primary_account}"
   instance_key_name = "${var.instance_key_name}"
   chef_boot_bucket = "${var.chef_boot_bucket}"
   chef_config_bucket = "${var.chef_config_bucket}"
